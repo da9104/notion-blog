262f582172c1073e1ed0fce39b008d1c3db42fe8
da9104 <dmk9104@gmail.com>
2025-09-16 23:09:36 +0900

    flex-col to flex-row

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,10 +7,9 @@ export function Footer() {
   return (
     <footer className="p-sides ">
       <div className="w-full md:h-[532px] h-[250px] p-sides md:p-11 text-background bg-black rounded-[12px] flex flex-col justify-between max-md:gap-8">
-        <div className="flex flex-col justify-between md:flex-row">
+        <div className="flex justify-between flex-row">
           <p className="md:text-[10rem] text-7xl font-bold shrink-0 text-white">©DAMI UI</p>
           {/* <ShopLinks collections={collections} className="max-md:hidden" align="right" /> */}
-
           <ToggleThemeButton
             options={[
               {
@@ -28,7 +27,6 @@ export function Footer() {
           {/* <span className="mt-3 italic font-semibold md:hidden text-white">Refined. Minimal. Never boring.</span> */}
         </div>
         <div className="flex justify-between max-md:contents ">
-
           {/* <SidebarLinks className="max-w-[450px] w-full max-md:flex-col" size="base" invert /> */}
           <p className="text-base text-white">{new Date().getFullYear()} — All rights reserved</p>
         </div>
